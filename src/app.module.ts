09ee482f1c3cb46d9a090d3ae413c9cc69c53c7b
acristoni <adonai_cn@hotmail.com>
2023-09-17 06:08:59 -0300

    FEAT: mongo config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './models/products.schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import mongooseConfig from './config/mongoose.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Carregue a configuração globalmente
+      load: [mongooseConfig], // Carregue a configuração do Mongoose
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('mongoose.uri'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+      imports: undefined,
+    }),
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
